736669180bfc1a938d717314219a3a071c0485f7
RUPENDER <97521113+RUPENDER30062003@users.noreply.github.com>
2024-02-05 16:52:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -312,7 +312,7 @@ app.use((err,req,res,next)=>{
   let{statusCode=500,message="something went wrong!"}=err;
   // res.status(statusCode).send(message);
   // res.render("error.ejs",{err});
-  res.status(statusCode).render("error.ejs",{message});
+  res.status(statusCode).render("error.ejs",{message,err});
 });
 
 
